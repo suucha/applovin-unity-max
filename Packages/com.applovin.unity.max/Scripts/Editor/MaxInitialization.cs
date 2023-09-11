92d6d74a8421500f0b4971a2bdc3ad21f1b85105
tubo <tubo@weitown.com>
2023-09-12 06:43:02 +0800

    为upm修改

--- Packages/com.applovin.unity.max/Scripts/Editor/MaxInitialization.cs
+++ Packages/com.applovin.unity.max/Scripts/Editor/MaxInitialization.cs
@@ -69,6 +69,16 @@ namespace AppLovinMax.Scripts.Editor
 
             var pluginParentDir = AppLovinIntegrationManager.PluginParentDirectory;
             var isPluginOutsideAssetsDir = AppLovinIntegrationManager.IsPluginOutsideAssetsDirectory;
+            //以下代码为upm包修改而增加的
+            var sourcePath = Path.GetFullPath(Path.Combine("Packages", "com.applovin.unity.max", "AppLovin", "Editor"));
+            var destPath = Path.Combine(pluginParentDir, "MaxSdk", "AppLovin", "Editor");
+            if (!Directory.Exists(destPath))
+            {
+                UnityEngine.Debug.Log($"Create dest path:{destPath}");
+                Directory.CreateDirectory(destPath);
+            }
+            File.Copy(Path.Combine(sourcePath, "Dependencies.xml"), Path.Combine(destPath, "Dependencies.xml"), true);
+            //为upm包修改结束
             var changesMade = AppLovinIntegrationManager.MovePluginFilesIfNeeded(pluginParentDir, isPluginOutsideAssetsDir);
             if (isPluginOutsideAssetsDir)
             {
